4db29ed0a0eeef1ea93c9f4dd3b8e18fd052078e
Cora Ioan-Alexandru <ionutcora@proton.me>
2025-11-14 12:40:47 +0200

    Navigation component reworked

--- FE/src/components/TopNav.tsx
+++ FE/src/components/TopNav.tsx
@@ -6,7 +6,7 @@ import energySvg from '../assets/green-energy.svg';
 import toolsSvg from '../assets/swords.svg';
 import userSvg from '../assets/User.svg';
 import { useAuth } from '../authentication/AuthContext';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './TopNav.css';
 import '../App.css';
 import { Typography } from '@mui/material';
@@ -31,7 +31,7 @@ export const TopNav = ({ energyLevel, onDecrease, onIncrease }: TopNavProps) => 
 
   return (
     <div className="topnav">
-      <div className="brand">
+      <div className="brand" onClick={() => navigate('/')} style={{cursor: 'pointer'}}>
         <img src={logoSvg} alt="TUDU Logo" className="brand-logo" />
         <Typography variant="h5" className="nanum-pen">TUDU</Typography>
       </div>
